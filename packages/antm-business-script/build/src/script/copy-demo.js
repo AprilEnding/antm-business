01f8b5794f7905bb63543d7f6a8a909279b022e7
AprilEnding <april_ending@qq.com>
2023-06-05 00:45:11 +0800

    复制demo

--- packages/antm-business-script/build/src/script/copy-demo.js
+++ packages/antm-business-script/build/src/script/copy-demo.js
@@ -1,16 +1,37 @@
-import glob from 'glob';
 import fs from 'fs';
 import path from 'path';
 export default function copyDemo() {
-    const demoFiles = glob.sync('../antm-business-demo/dist/**/*.*');
-    console.log('dmeo files', demoFiles);
-    if (demoFiles?.length) {
-        demoFiles.forEach((item) => {
-            let sitePublicPath = item.replace('../antm-business-demo/dist/', 'public/');
-            sitePublicPath = sitePublicPath.replace(/index\.html/, 'h5.html');
-            fs.copyFileSync(path.resolve(process.cwd(), item), path.resolve(process.cwd(), sitePublicPath));
-        });
+    const demoBaseFile = path.resolve(process.cwd(), '../antm-business-demo/dist');
+    const sitePublicFile = path.resolve(process.cwd(), 'public');
+    const replaceTargetFile = (_, targetPath) => {
+        if (targetPath === path.resolve(sitePublicFile, 'index.html')) {
+            return path.resolve(sitePublicFile, 'h5.html');
+        }
+        else {
+            return targetPath;
+        }
+    };
+    copy(demoBaseFile, sitePublicFile, { replaceTargetFile: replaceTargetFile });
+}
+function copy(source, target, option) {
+    const result = fs.readdirSync(source, { withFileTypes: true });
+    const { replaceTargetFile } = option || {};
+    if (!fs.existsSync(target)) {
+        fs.mkdirSync(target);
     }
-    else {
+    if (result?.length) {
+        result.forEach((dirObj) => {
+            const sourcePath = path.resolve(source, dirObj.name);
+            const targetPath = typeof replaceTargetFile === 'function'
+                ? replaceTargetFile(sourcePath, path.resolve(target, dirObj.name))
+                : path.resolve(target, dirObj.name);
+            if (dirObj.isDirectory()) {
+                !fs.existsSync(targetPath) && fs.mkdirSync(targetPath);
+                copy(sourcePath, targetPath);
+            }
+            else {
+                fs.copyFileSync(sourcePath, targetPath);
+            }
+        });
     }
 }
