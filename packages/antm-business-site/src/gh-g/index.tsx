a704d3494ecc94296946b639a472fa2253147c4e
AprilEnding <april_ending@qq.com>
2023-06-13 02:07:55 +0800

    change gitignore

--- packages/antm-business-site/src/gh-g/index.tsx
+++
@@ -1,3 +0,0 @@
-import React, { type FC } from 'react';
-const GhG: FC<{ title: string }> = (props) => <h4>{props.title}</h4>;
-export default GhG;
--- packages/antm-business-site/src/index.ts
+++ packages/antm-business-site/src/index.ts
@@ -1,7 +1,2 @@
 export { default as TagList } from './tag-list';
 export { default as HomeSkeleton } from './home-skeleton';
-export { default as Hhj } from './hhj';
-export { default as KllGh } from './kll-gh';
-export { default as KlHj } from './kl-hj';
-export { default as HjKl } from './hj-kl';
-export { default as GhG } from './gh-g';
